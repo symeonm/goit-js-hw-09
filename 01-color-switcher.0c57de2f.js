fc0cad8566ec0e482ef2db588dcbdbb81bb6618c
symeonm <116294801+symeonm@users.noreply.github.com>
2023-04-16 14:02:48 +0000

    Deploying to gh-pages from @ symeonm/goit-js-hw-09@11a33c5a35b1233c78be68dfb0f03ce770ef9d60 🚀

---
+++ 01-color-switcher.0c57de2f.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("button[data-start]");const t=document.querySelectorAll("button[data-stop]");console.log(t);
+//# sourceMappingURL=01-color-switcher.0c57de2f.js.map
---
+++ 01-color-switcher.97478b19.js
@@ -0,0 +1,2 @@
+!function(){document.querySelectorAll("button[data-start]");var t=document.querySelectorAll("button[data-stop]");console.log(t)}();
+//# sourceMappingURL=01-color-switcher.97478b19.js.map
